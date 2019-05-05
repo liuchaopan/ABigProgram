2e3bd2a3bd2bc4b12e845e1b8355c29b6193c7e7
liuchaopan <jj>
2019-05-05 22:54:56 +0800

    fix kotlin compile error

--- app/src/main/java/com/pan/abigprogram/ScrollingActivity.kt
+++ app/src/main/java/com/pan/abigprogram/ScrollingActivity.kt
@@ -1,15 +1,14 @@
 package com.pan.abigprogram
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_scrolling2.*
+import com.google.android.material.snackbar.Snackbar
 
 class ScrollingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_scrolling2)
+        setContentView(R.layout.activity_scrolling)
         setSupportActionBar(toolbar)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
